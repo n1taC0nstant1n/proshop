bc6c19246fd3feff041bca75af13256a5b9f014e
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-01 13:39:24 +0300

    04 preparing sample data

---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+import bcryptjs from 'bcryptjs'
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcryptjs.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: bcryptjs.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        password: bcryptjs.hashSync('123456', 10),
+        isAdmin: true
+    }
+]
