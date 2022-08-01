b0d5b30324ba69a40d4fc5f670affa7e68151251
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-01 11:45:15 +0300

    04 connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@
 // const products = require('./data/products')
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 
 dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
