81690ad091201260f946b7821c6af016bd6a27df
Nita Constantin <nita.constantin880722@yahoo.com>
2022-08-05 11:45:48 +0300

    05 product list reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,10 @@
 import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-
-const reducer = combineReducers({})
+import {productListReducers} from './reducers/productReducers'
+const reducer = combineReducers({
+    productList: productListReducers
+})
 
 const initialState = {}
 
